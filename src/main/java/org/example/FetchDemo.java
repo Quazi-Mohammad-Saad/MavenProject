0c0fed4610931cffcbd5ba53a1a429bbdea4e607
Quazi <quazi1996saad@gmail.com>
2021-04-07 11:38:46 +0530

    added get() and load() method

--- src/main/java/org/example/FetchDemo.java
+++ src/main/java/org/example/FetchDemo.java
@@ -14,8 +14,19 @@ public class FetchDemo {
         Session session=factory.openSession();
 
         //get-student :1,7 or 8
-        Address student=(Address) session.get(Address.class,1);
-        System.out.println(student);
+        Student student=(Student) session.load(Student.class,1);   //Student.class --->Identity Type
+//        System.out.println(student);
+        //like in which class's object you want to find followed by Identifier
+//        System.out.println(student.getCity()+" : "+student.getName());
+
+        //Creating other objects of Student class
+//        Student student1=(Student) session.get(Student.class,1);
+//        System.out.println(student);
+
+
+        //Same thing we are performing for the Address class
+//        Address ad = (Address) session.get(Address.class,1);
+//        System.out.println(ad.getCity()+" : "+ad.isOpen());
 
         session.close();
         factory.close();
